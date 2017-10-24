900cbfad737a54b55abfd6dff263eb41b6941794
Luiz <lcsjr.s23@gmail.com>
2017-10-24 07:39:42 -0200

    Melhorias no exemplo do código BREAK e CONTINUE

--- src/com/javabasico/basico01/Break_Continue.java
+++ src/com/javabasico/basico01/Break_Continue.java
@@ -8,10 +8,10 @@ public class Break_Continue {
 		
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.print("Entre com um número: ");
+		System.out.print("Informe o valor do \'Dividor\' : ");
 		int num = scan.nextInt();
 		
-		System.out.print("Entre com um limite: ");
+		System.out.print("Informe o \'Dividendo\': ");
 		int max = scan.nextInt();
 		
 		/*
@@ -19,20 +19,23 @@ public class Break_Continue {
 		 */
 		for ( int i=num; i<=max; i++) {
 			
-			if (i % 7 == 0) {
-				System.out.println("O valor de i é: " + i);
+			if (i % 5 == 0) {
+				System.out.println("BREAK: Quando \'Resto\' é igual a Zero, o valor de i é: " + i);
 				break;
 			}
 		}
 		
+		System.out.println("--------------------------------------");
 		/*
 		 * CONTINUE 
 		 */
 		for ( int i=num; i<=max; i++) {
 			
-			if (i % 7 == 0) {
+			if (i % 5 == 0) {
+				System.out.println("CONTINUE: Quando \'Resto\' é igual a Zero, o valor de i é: " + i);
 				continue; // equivalente ao comando LOOP 
 			}
+			System.out.println("CONTINUE: Quando \'Resto\' é diferente de Zero, o valor de i é: " + i);
 		}
 			
 	scan.close();
