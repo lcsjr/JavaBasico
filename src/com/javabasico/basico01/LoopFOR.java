54d9a7e694d485a3a7252ea2b4ad1101736a92ac
Luiz <lcsjr.s23@gmail.com>
2017-10-24 00:07:07 -0200

    class FOR com diversos cenários

---
+++ src/com/javabasico/basico01/LoopFOR.java
@@ -0,0 +1,40 @@
+package com.javabasico.basico01;
+
+public class LoopFOR {
+
+	public static void main(String[] args) {
+		
+		// incrementando
+		for (int i = 0; i < 5; i++) {
+			System.out.println("incrementando em i: " + i);
+		}
+		
+		// decrementando
+		for (int i = 5; i > 0; i--) {
+			System.out.println("decrementando em i: " + i);
+		}
+		
+		//incrementando de 5 em 5
+		for (int i = 0; i <= 25; i+=5) {
+			System.out.println("incremento de 5 em i: " + i);
+		}
+		
+		//utilizando mais de uma variável de controle no FOR
+		for (int i=0, j=10 ; i<j ; i++, j--) {
+			System.out.println("i = " + i + "; j = " +j);
+		}
+		
+		// inicializando variavel contador fora do FOR
+		int count = 0;
+		for ( ; count < 10; count += 2 ) {
+			System.out.println("inicializando contador fora do FOR: " + count);
+		}
+		System.out.println("Valor final de count: " + count);		
+		
+		// utilizando FOR para soma sem uso de chaves
+		int soma = 0;
+		for (int i=1; i < 5; soma += i++);
+		System.out.println("O Resultado desta soma é: "+soma);
+	}
+
+}
