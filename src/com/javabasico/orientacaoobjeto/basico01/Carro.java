e770737c21000893bc94f06b88698ffc478028ce
Luiz <lcsjr.s23@gmail.com>
2017-10-26 19:26:40 -0200

    Primeira classe com apenas atributos

---
+++ src/com/javabasico/orientacaoobjeto/basico01/Carro.java
@@ -0,0 +1,11 @@
+package com.javabasico.orientacaoobjeto.basico01;
+
+public class Carro {
+	
+	String marca;
+	String modelo;
+	int numPassageiros;
+	double capacidadeCombustivel;
+	double consumoCombustivel;
+
+}
---
+++ src/com/javabasico/orientacaoobjeto/basico01/TesteCarro.java
@@ -0,0 +1,40 @@
+package com.javabasico.orientacaoobjeto.basico01;
+
+/*
+ * Exercícios: https://www.slideshare.net/loianeg/curso-java-basico-exercicios-aula-24
+ */
+public class TesteCarro {
+
+	public static void main(String[] args) {
+		
+		Carro hond = new Carro();
+		hond.marca = "Honda";
+		hond.modelo = "New Civic";
+		hond.numPassageiros = 5;
+		hond.capacidadeCombustivel = 100;
+		hond.consumoCombustivel = 0.2;
+		
+		
+		System.out.println("Endereço na memória: " +hond);
+		
+		System.out.println("Marca: " + hond.marca);
+		System.out.println("Modelo: "+ hond.modelo);
+		
+		System.out.println();
+		
+		Carro mit = new Carro();
+		mit.marca = "Mitshubishi";
+		mit.modelo = "ASX";
+		mit.numPassageiros = 5;
+		mit.capacidadeCombustivel = 60;
+		mit.consumoCombustivel = 0.2;
+		
+		
+		System.out.println("Endereço na memória: " +mit);
+		
+		System.out.println("Marca: " + mit.marca);
+		System.out.println("Modelo: "+ mit.modelo);
+		
+	}
+
+}
